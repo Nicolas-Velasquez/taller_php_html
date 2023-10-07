8f374b1a35d6043097a47f034029c869958fefd4
Nicolas-Velasquez <125744775+Nicolas-Velasquez@users.noreply.github.com>
2023-10-06 22:04:12 -0500

    Se agregó la función para de ordenar los numeros de la lista de menor a mayor

--- punto1.php
+++ punto1.php
@@ -11,16 +11,42 @@
 
 <?php
 
-$lista = [7, 62, 3, 61, 6, 70, 23, 27, 8, 13, 67];
+$lista = [7, 62, 3, 61, 6, 70, 23, 27, 8, 13, 67];//lista original
 
 
-for($i=0; $i<count($lista); $i++){//no terminado
-    if($lista[$i] < $lista[$i+1]){
+echo '<br> Lista original:';
+foreach($lista as $num){
+    echo '<br>'.$num;
+}
 
+echo '<br><br> Lista ordenada: ';
+ordenar($lista);
+
+function ordenar($lista){//ya funciona
+    $num = 0;
+    $ordenado = $lista;
+    for($i=0; $i<count($ordenado); $i++){
+        for($j=$i; $j<count($ordenado); $j++){
+            if($ordenado[$j] < $ordenado[$i]){
+                $num = $ordenado[$i];
+                $ordenado[$i] = $ordenado[$j];
+                $ordenado[$j] = $num;
+            }
+        }
+    }
+    foreach($ordenado as $n){
+        echo '<br>'.$n;
     }
 }
 
-/*
+function pares($lista, $pares){
+    $pares = [];
+}
+
+function impares($lista, $impares){
+    $impares = [];
+}
+/*ordenado
 3
 6
 7
@@ -34,4 +60,20 @@ for($i=0; $i<count($lista); $i++){//no terminado
 70
 */
 
+/*pares
+70
+62
+8
+6
+*/
+
+/*impares
+3
+7
+13
+23
+27
+61
+67
+*/
 ?>
